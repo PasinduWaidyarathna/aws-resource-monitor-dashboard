6a4122b9480404c4500a1bc53b3d2ba6a82f9e57
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2025-04-22 10:55:07 +0530

    Update aws-monitor.sh

--- aws-monitor.sh
+++ aws-monitor.sh
@@ -28,9 +28,13 @@ echo "" >> "$REPORT_FILE"
 # EC2 Instances
 if [[ " ${SERVICES[@]} " =~ " ec2 " ]]; then
     echo ">>> EC2 Instances:" >> "$REPORT_FILE"
+    echo "| Instance ID        | Instance Type | State      | Public IP        |" >> "$REPORT_FILE"
+    echo "|---------------------|---------------|------------|------------------|" >> "$REPORT_FILE"
     aws ec2 describe-instances \
         --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name,PublicIpAddress]' \
-        --region "$REGION" --output table >> "$REPORT_FILE" 2>>"$REPORT_FILE"
+        --region "$REGION" --output text | while read -r id type state ip; do
+        printf "| %-20s | %-13s | %-10s | %-16s |\n" "$id" "$type" "$state" "${ip:--}" >> "$REPORT_FILE"
+    done
     echo "" >> "$REPORT_FILE"
 fi
 
